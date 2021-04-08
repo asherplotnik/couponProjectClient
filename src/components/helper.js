ce31d1cb3f84f4fd12a02135bf19d718c49e2bca
Asher <asherplotnik@gmail.com>
2021-04-08 20:23:47 +0700

    typescript

--- src/components/helper.js
+++
@@ -1,5 +0,0 @@
-export const localUrl = "http://192.168.1.100";
-//export const localUrl = "http://localhost";
-
-export const boxShadow =
-  "0 4px 8px 0 rgba(189, 187, 187, 0.2), 0 3px 10px 0 rgba(83, 83, 83, 0.19)";
