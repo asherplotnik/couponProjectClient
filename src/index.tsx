ccc386041776fe0182322ed3b2d97137468ce0ac
Asher <asherplotnik@gmail.com>
2021-04-22 11:54:01 +0700

    GOOD

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Layout from "./components/layoutArea/layout";
+import Layout from "./Components/LayoutArea/Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
