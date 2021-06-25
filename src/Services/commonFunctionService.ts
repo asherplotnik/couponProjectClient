b9d9f4d0d9db6ea43691d2d48c92e3c94c468d37
Asher <asherplotnik@gmail.com>
2021-06-25 22:17:39 +0700

    console

--- src/Services/commonFunctionService.ts
+++ src/Services/commonFunctionService.ts
@@ -1,5 +1,6 @@
 export const errorAlert = (error:any) => {
     if (error.response){
+        console.assert(error?.response?.data?.message);
         alert(error?.response?.data?.message);
     }else {
         alert("ERROR !!!");
